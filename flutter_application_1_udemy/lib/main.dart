a5a9571aa6af4e466f8a5e3764d02f8bf6dc973c
Yash Lad <yashlad727@gmail.com>
2022-08-07 22:13:16 +0530

    adding column and raised buttons

--- flutter_application_1_udemy/lib/main.dart
+++ flutter_application_1_udemy/lib/main.dart
@@ -4,12 +4,40 @@ void main() {
   runApp(MyApp());
 }
 
+// void main() => runApp(MyApp());
+// short hand notation ( only for functions with one line of expressio )
 class MyApp extends StatelessWidget {
+  @override
   // adding a method not properties
   // in flutter whole app is a widget!
   Widget build(BuildContext context) {
+    var questions = [
+      "What's your favourite color?",
+      'What\'s your favourite animal?',
+    ];
     return MaterialApp(
-      home: Text('Hello!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(children: [
+          Text('The Questions!'),
+          RaisedButton(
+            child: Text('Answer-1'),
+            onPressed: null,
+          ),
+          RaisedButton(
+            child: Text('Answer-2'),
+            onPressed: null,
+          ),
+          RaisedButton(
+            child: Text('Answer-3'),
+            onPressed: null,
+          ),
+          // raised button is depracated!!!
+          // ElevatedButton(onPressed: onPressed, child: child)
+        ]),
+      ),
     ); // named arguments in materialApp()
   }
 }
