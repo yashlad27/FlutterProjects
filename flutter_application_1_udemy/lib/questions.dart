f90c3ec9781bc2880bca7cf8dce71435bc96b566
Yash Lad <yashlad727@gmail.com>
2022-08-10 15:45:26 +0530

    indentation

--- flutter_application_1_udemy/lib/questions.dart
+++ flutter_application_1_udemy/lib/questions.dart
@@ -4,7 +4,7 @@ import 'package:flutter/material.dart';
 
 class Question extends StatelessWidget {
   //const Question({Key? key}) : super(key: key);
-  String questionText;
+  final String questionText;
 
   Question(this.questionText);
 
