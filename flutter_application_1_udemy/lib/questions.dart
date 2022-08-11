1d18bf14da1fe4f3c3405b1130b9b383d276b3d4
Yash Lad <yashlad727@gmail.com>
2022-08-11 21:21:39 +0530

    answer.dart file created

--- flutter_application_1_udemy/lib/questions.dart
+++ flutter_application_1_udemy/lib/questions.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors, override_on_non_overriding_member, use_key_in_widget_constructors, prefer_const_constructors_in_immutables, unnecessary_import, unused_import
+
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
 import 'package:flutter/material.dart';
